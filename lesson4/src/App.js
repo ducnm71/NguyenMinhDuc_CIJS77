291f3c9ee7a2f7378c3e0defa0e20dff20b3d5e9
ducnm71 <21021293@vnu.edu.vn>
2022-11-01 16:33:38 +0700

    Lesson 4 & 5

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -1,86 +1,72 @@
 
 import './App.css';
-import CounterFC from './Components/Counter';
-import StudentFC from './Components/Student';
-import TrafficLight from './Components/TrafficLight';
+// import CounterFC from './Components/Counter';
+// import StudentFC from './Components/Student';
+// import TrafficLight from './Components/TrafficLight';
 import React from 'react'
+import RegisterPage from './Components/Form';
 
 function App() {
   return (
     <div className="App">
-      <div className='App-header'>
-        <div>
-          (Function Component)
-          <CounterFC/>
-          <StudentFC/>
-        </div>
-        
-        <div>
-          (Class Component)
-          <CounterCC/>
-          <StudentCC/>
-        </div>
-
-        <TrafficLight/>
-      </div>
+        <RegisterPage/>
     </div>
   );
 }
 
 export default App;
 
-class CounterCC extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {count: 0}
-    this.clickCount = this.clickCount.bind(this)
-  }
-  clickCount () {
-    this.setState({
-      ...this.state,
-      count: this.state.count+1
-    })
-  }
-  render() {
-    return(
-      <div>
-        <h4>{this.state.count}</h4>
-        <button onClick={this.clickCount}>Increase</button>
-      </div>
-    )
-  }
-}
-class StudentCC extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      student: {
-        name: 'Nguyen Minh Duc',
-        age: 19
-      }
+// class CounterCC extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {count: 0}
+//     this.clickCount = this.clickCount.bind(this)
+//   }
+//   clickCount () {
+//     this.setState({count: this.state.count+1})
+//   }
+//   render() {
+//     return(
+//       <div>
+//         <h4>{this.state.count}</h4>
+//         <button onClick={this.clickCount}>Increase</button>
+//       </div>
+//     )
+//   }
+// }
+// class StudentCC extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       student: {
+//         name: 'Nguyen Minh Duc',
+//         age: 19
+//       }
       
-    }
-    this.clickStudent = this.clickStudent.bind(this)
-  }
+//     }
+//     this.clickStudent = this.clickStudent.bind(this)
+//   }
 
-  clickStudent () {
-    this.setState({
-      ...this.state,
-      student: {
-        ...this.state.student,
-        age: this.state.student.age + 1
-      }
-    })
-  }
+//   clickStudent () {
+//     this.setState({
+//       ...this.state,
+//       student: {
+//         ...this.state.student,
+//         age: this.state.student.age + 1
+//       } 
+//     })
+//   }
 
 
-  render() {
-    return (
-      <div>
-        <h4>Name: {this.state.student.name}. Age: {this.state.student.age}</h4>
-        <button onClick={this.clickStudent}>Click me</button>
-      </div>
-    )
-  }
+//   render() {
+//     const {student, clickStudent} = this.state;
+//     return (
+      
+//       <div>
+//         <h4>Name: {student.name}. Age: {student.age}</h4>
+//         <button onClick={clickStudent}>Click me</button>
+//       </div>
+//     )
+//   }
   
-}
+// }
---
+++ lesson4/src/Components/Form/index.js
@@ -0,0 +1,147 @@
+import './style.scss'
+import React from 'react'
+
+class RegisterPage extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        fullname: "",
+        username: "",
+        password: "",
+        confirm: "",
+        errorMessage: {
+          fullname: "",
+          username: "",
+          password: "",
+          confirm: "",
+        },
+        allItem: []
+      }
+    }
+    checkExist = () => {
+      const {username} = this.state
+      fetch("https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users")
+      .then(res => res.json())
+      .then(item => {
+        console.log(item);
+
+      })
+      console.log(username);
+    }
+    
+    onChangeInput = (nameInput, value) => {
+      const errorMessage = {
+        ...this.state.errorMessage,
+      }
+      const nameRegex = /^[a-zA-Z\-]+$/;
+      const validFullname = nameRegex.test(this.state.fullname)
+      if(validFullname === false){
+        errorMessage['fullname'] = "Your fullname or username is not valid"
+      }else{
+        errorMessage['fullname'] = "" 
+      }  
+      const validUsername = nameRegex.test(this.state.username)
+      if(validUsername === false){
+        errorMessage['username'] = "Your username or username is not valid"
+      }else{
+        errorMessage['username'] = ""
+      }
+      if (nameInput === "confirm" && this.state.password !== value) {
+        errorMessage['confirm'] = "Password is not the same";
+      } else {
+        errorMessage['confirm'] = "";
+      }
+      this.setState({
+        ...this.state,
+        [nameInput]: value,
+        errorMessage: errorMessage,
+      })
+      console.log(value);
+    }
+
+    handleSubmitForm = (e) => {
+      e.preventDefault();
+      const { errorMessage, fullname, password, username } = this.state
+      if (Object.values(errorMessage).filter((value) => value !== "").length > 0) {
+        return;
+      }
+      fetch("https://635d3184cb6cf98e56af2894.mockapi.io/api/v1/users", {
+        method: "POST",
+        body: JSON.stringify({
+          user: {
+            name: fullname,
+            username: username,
+            password: password,
+          }
+        })
+      });
+      this.checkExist()
+      this.setState({...this.state,
+        fullname: "",
+        username: "",
+        password: "",
+        confirm: "",
+        errorMessage: {
+          fullname: "",
+          username: "",
+          password: "",
+          confirm: "",
+        }
+      })
+    }
+    
+    render() {
+      const { errorMessage } = this.state
+      return (
+      <div className='register-form'>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmitForm}>
+                    <div className='form__group field'>
+                        
+                        <input className='form__field' value={this.state.fullname} type="text"  name="fullname" placeholder='Fullname' onChange={(e) => {
+                        this.onChangeInput("fullname", e.target.value);
+                        }} />
+                        <label className='form__label' for='fullname'>Fullname</label>
+                    </div>
+                    <div>
+                      {errorMessage.fullname !== "" ? <div className='error'>{errorMessage.fullname}</div> : <></>}
+                    </div>
+                
+                    <div className='form__group field'>
+                        
+                        <input className='form__field' value={this.state.username} type="text" name="username" placeholder='Username' onChange={(e) => {
+                        this.onChangeInput("username", e.target.value);
+                    }} />
+                        <label className='form__label' for='username'>Username</label>
+                    </div>
+                    <div>
+                      {errorMessage.username !== "" ? <div className='error'>{errorMessage.username}</div> : <></>}
+                    </div>
+                
+                    <div className='form__group field'>
+                        
+                        <input className='form__field' value={this.state.password} type="password" name="password" placeholder='Password' onChange={(e) => {
+                        this.onChangeInput("password", e.target.value);
+                    }} />
+                        <label className='form__label' for='password'>Password</label>
+                    </div>
+                
+                
+                    <div className='form__group field'>
+                        
+                        <input className='form__field' value={this.state.confirm} type="password" name='confirm' placeholder='Confirm Password' onChange={(e) => {
+                        this.onChangeInput("confirm", e.target.value);
+                    }} />
+                        <label className='form__label' for='confirm'>Confirm Password</label>
+                    </div>
+                        {errorMessage.confirm !== "" ? <div className='error'>{errorMessage.confirm}</div> : <></>}
+                    <div>
+                <button type='submit'>Submit</button>
+            </div>
+        </form>
+      </div>
+      )
+    }
+  }
+
+  export default RegisterPage
